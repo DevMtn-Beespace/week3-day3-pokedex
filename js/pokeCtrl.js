d9d2f9ab95f967e4a4edbafed4b90576e02c41f7
mlecke <mlecke@beespace.com>
2016-07-13 17:08:56 -0600

    pokemon

--- js/pokeCtrl.js
+++ js/pokeCtrl.js
@@ -2,7 +2,11 @@ angular.module('pokedex')
   .controller('pokeCtrl', function ($scope, pokeServ) {
 
 //manipulate api data here
-
-
+$scope.pokeData = function(num) {
+  pokeServ.getPokemon(num).then(function(response){
+    console.log(response);
+    $scope.pokepic = response.sprites.front_shiny;
+  });
+};
 
   });
